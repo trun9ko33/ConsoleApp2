3686868bbc6750bfec1ce788a1fb69addfb4e4b9
Trung <trun9ko33@gmail.com>
2023-05-24 15:46:34 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+
+var n = 0;
+var v = new List<customer>();
+var c = 1;
+while (c > 0 && c < 5)
+{
+    Console.WriteLine("nhap lua chon cua ban: ");
+    Console.WriteLine("1. nhap n khac hang");
+    Console.WriteLine("2. tinh thanh tien");
+    Console.WriteLine("3 sap xep theo thu tu giam dan");
+    Console.WriteLine("4 in ra ");
+    Console.WriteLine("0 thoat");
+    c = int.Parse(Console.ReadLine());
+
+    switch (c)
+    {
+        case 1:
+            Console.WriteLine("nhap so luong khach hang");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("nhap khach hang thu" + i);
+                Console.WriteLine("Loai KH(CN,CT,NN)");
+                var cName = (Console.ReadLine());
+                Console.WriteLine("ten hang");
+                var iName = Console.ReadLine();
+                Console.WriteLine("so luong");
+                var q = int.Parse(Console.ReadLine());
+                Console.WriteLine("don gia");
+                var p = double.Parse(Console.ReadLine());
+                v.Add(new customer(cName, iName, q, p));
+            }
+
+            break;
+
+        case 2:
+            {
+                var temp = 0;
+
+                foreach (customer customer in v)
+                    customer.getCost();
+
+
+                break;
+            }
+        case 3:
+            {
+                customer temp = null;
+
+                for (int i = 0; i < v.Count; i++)
+                {
+                    for (int j = i + 1; j < v.Count; j++)
+                    {
+                        if (v[i].Price.CompareTo(v[j].Price) <= 0)
+                        {
+                            temp = v[i];
+                            v[i] = v[j];
+                            v[j] = temp;
+                        }
+                    }
+                }
+
+                break;
+            }
+        case 4:
+            double sum = 0;
+            Console.WriteLine("khach hang \t ten hang hoa \t so luon \t don gia \t thanh tien");
+            foreach (customer customer in v)
+            {
+                sum += customer.Cost;
+                Console.WriteLine(customer.CustomerName + "\t" + customer.ItemName + "\t" + customer.Qty + "\t" + customer.Price + "\t" + customer.Cost);
+            }
+
+            Console.WriteLine("tong cong thanh tien \t:" + sum);
+            break;
+    }
+}
---
+++ ConsoleApp2/customer.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp2
+{
+    public class customer
+    {
+        private string customerType = "";
+        private string itemName = "";
+        private int qty = 0;
+        private double price = 0;
+        private double cost = 0;
+
+        public customer(string customerName, string itemName, int qty, double price)
+        {
+            this.customerType = customerName;
+            this.itemName = itemName;
+            this.qty = qty;
+            this.price = price;
+        }
+
+        public string CustomerName { get => customerType; set => customerType = value; }
+        public string ItemName { get => itemName; set => itemName = value; }
+        public int Qty { get => qty; set => qty = value; }
+        public double Price { get => price; set => price = value; }
+        public double Cost { get => cost; set => cost = value; }
+
+        //public double Cost { get => cost; set => cost = value; }
+        public void getCost()
+        {
+            double c = price;
+            if (customerType.Contains("CN"))
+                this.Cost = qty * c * 0.93;
+            else if (customerType.Contains("CT"))
+                this.Cost = qty * c * 0.96;
+            else this.Cost = qty * c;
+        }
+    }
+}
